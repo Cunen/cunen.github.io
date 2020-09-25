aaa81100ad2e3876a58242a4b3c0877ba7239d06
Kalle Kuparinen <kalle.kuparinen@tuni.fi>
2020-09-25 18:11:21 +0300

    Updates

---
+++ precache-manifest.b64e0ea0e140167a31695cc4f73ab1ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4673181a517fffb16b12c851ea851b6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d50eb1185ef690922e5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f52491dd9d5cac72c967",
+    "url": "/static/js/2.a0f15559.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0f15559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d50eb1185ef690922e5",
+    "url": "/static/js/main.e07ad5b1.chunk.js"
+  },
+  {
+    "revision": "94bca08fb99f78848146",
+    "url": "/static/js/runtime-main.99904550.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
