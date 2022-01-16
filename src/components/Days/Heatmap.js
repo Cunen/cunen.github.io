7e30c3d2316665bb35ef53e53399b4a268b0baa9
Kalle Kuparinen <kalle.kuparinen@tuni.fi>
2022-01-16 11:11:45 +0200

    Add ultra colors

--- src/components/Days/Heatmap.js
+++ src/components/Days/Heatmap.js
@@ -2,17 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const getColorFromCalories = (cal) => {
+  if (cal >= 1000) return `hsl(166, 100%, 50%)`;
+  else if (cal >= 800) return `hsl(145, 100%, 50%)`;
+  else if (cal >= 600) return `hsl(133, 100%, 50%)`;
+
   const excellentResult = 500;
   const hslMax = 133;
   const result = Math.max(0, Math.min(1, cal / excellentResult));
-  return `hsl(${hslMax * result}, 54%, 43%)`;
+  return `hsl(${hslMax * result}, 50%, 40%)`;
 };
 
 export const getColorFromTime = (time) => {
+  if (time >= 14400) return `hsl(166, 100%, 50%)`;
+  else if (time >= 10800) return `hsl(145, 100%, 50%)`;
+  else if (time >= 7200) return `hsl(133, 100%, 50%)`;
+
   const excellentResult = 5400;
   const hslMax = 133;
   const result = Math.max(0, Math.min(1, time / excellentResult));
-  return `hsl(${hslMax * result}, 54%, 43%)`;
+  return `hsl(${hslMax * result}, 50%, 40%)`;
 };
 
 function Heatmap({ days, height, maxWidth, mode }) {
@@ -31,7 +39,8 @@ function Heatmap({ days, height, maxWidth, mode }) {
     const steps = days.map((day, i) => {
       const sum = mode === 'calories' ? getKcalSum(day) : getTimeSum(day);
       const point = (i / len * 100) + shift;
-      return `${getColorFromCalories(sum)} ${point.toFixed(1)}%`;
+      const color = mode === 'calories' ? getColorFromCalories(sum) : getColorFromTime(sum);
+      return `${color} ${point.toFixed(1)}%`;
     });
     return steps.join(', ');
   }
