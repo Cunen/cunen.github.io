3a43c5b889ce4035536d129f4e5cd603714a5116
Kalle Kuparinen <kkuparinen@topcon.com>
2023-01-30 14:11:40 +0200

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
-import './App.css';
-import React from 'react';
-import Stats from './components/Stats';
-import styled from 'styled-components';
+import "./App.css";
+import React from "react";
+import Stats from "./components/Stats";
+import styled from "styled-components";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { initializeApp } from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import { collection, getDocs, getFirestore } from 'firebase/firestore';
-import Login from './components/Login';
-import { NavLink, Route, Switch } from 'react-router-dom';
-import { AppBar, IconButton, Menu, MenuItem, Select, Toolbar, Typography } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import MenuIcon from '@mui/icons-material/Menu';
-import { getAuth } from 'firebase/auth';
-import Import from './components/Import';
-import AuthCodePage from './components/AuthCodePage';
-import Profile, { profileIsAuthorized } from './components/Profile';
-import Dashboard from './components/Dashboard';
-import Recap from './components/Recap';
-import Map from './components/Map';
-import Guest, { GuestDialog } from './components/Guest';
-
+import { initializeApp } from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import Login from "./components/Login";
+import { NavLink, Route, Switch } from "react-router-dom";
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MenuIcon from "@mui/icons-material/Menu";
+import { getAuth } from "firebase/auth";
+import Import from "./components/Import";
+import AuthCodePage from "./components/AuthCodePage";
+import Profile, { profileIsAuthorized } from "./components/Profile";
+import Dashboard from "./components/Dashboard";
+import Recap from "./components/Recap";
+import Map from "./components/Map";
+import Guest, { GuestDialog } from "./components/Guest";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
 const firebase = initializeApp({
-  apiKey: 'AIzaSyDuLhFU-X9QUqlx0Cw1sn92eAtB7pSOSk0',
-  authDomain: 'housework-a9d38.firebaseapp.com',
-  databaseURL: 'https://housework-a9d38.firebaseio.com',
-  projectId: 'housework-a9d38',
-  storageBucket: 'housework-a9d38.appspot.com',
-  messagingSenderId: '475852222396',
-  appId: '1:475852222396:web:9fa6485a42ba1284e985b9',
-  measurementId: 'G-ZVBWH6CY7R'
+  apiKey: "AIzaSyDuLhFU-X9QUqlx0Cw1sn92eAtB7pSOSk0",
+  authDomain: "housework-a9d38.firebaseapp.com",
+  databaseURL: "https://housework-a9d38.firebaseio.com",
+  projectId: "housework-a9d38",
+  storageBucket: "housework-a9d38.appspot.com",
+  messagingSenderId: "475852222396",
+  appId: "1:475852222396:web:9fa6485a42ba1284e985b9",
+  measurementId: "G-ZVBWH6CY7R",
 });
 
 export const auth = getAuth();
@@ -45,21 +52,23 @@ const db = getFirestore(firebase);
 
 const getCodeFromWindowSearch = () => {
   const search = window.location.search;
-  const replaced = search.replace('?', '');
-  const split = replaced.split('&');
-  const obj = {}
-  split.forEach(s => {
-    const equalSplit = s.split('=');
+  const replaced = search.replace("?", "");
+  const split = replaced.split("&");
+  const obj = {};
+  split.forEach((s) => {
+    const equalSplit = s.split("=");
     obj[equalSplit[0]] = equalSplit[1];
   });
   return obj.code;
-}
+};
 
 function App() {
   // Get tracked lol
-  const tracker = localStorage.getItem('cunen-is-tracking-you');
+  const tracker = localStorage.getItem("cunen-is-tracking-you");
 
-  const [user, setUser] = React.useState(tracker ? JSON.parse(tracker) : auth.currentUser);
+  const [user, setUser] = React.useState(
+    tracker ? JSON.parse(tracker) : auth.currentUser
+  );
   const [guest, setGuest] = React.useState(null);
 
   const [menuAnchor, setMenuAnchor] = React.useState(null);
@@ -79,33 +88,35 @@ function App() {
     const auth = getAuth();
     auth.signOut();
     setUser(undefined);
-    localStorage.removeItem('cunen-is-tracking-you');
-  }
+    localStorage.removeItem("cunen-is-tracking-you");
+  };
 
   const code = getCodeFromWindowSearch();
 
   // Load user activities when User changes
   React.useEffect(() => {
     if (!user && !guest) return;
-    const currentUserCollection = 'activities-' + (guest || user.uid)
+    const currentUserCollection = "activities-" + (guest || user.uid);
     const dbRef = collection(db, currentUserCollection);
-    getDocs(dbRef).then(res => {
-      const acts = res.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    getDocs(dbRef).then((res) => {
+      const acts = res.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
       setActivities(acts);
-    })
+    });
   }, [guest, user]);
 
   React.useState(() => {
     window.setTimeout(() => setUser(auth.currentUser), 2000);
-  }, [auth])
+  }, [auth]);
 
   // Get activities for selected year
   React.useEffect(() => {
-    setYearActivities(activities.filter(a => {
-      const date = a.date.toDate();
-      return date.getFullYear() === year;
-    }));
-  }, [activities, year])
+    setYearActivities(
+      activities.filter((a) => {
+        const date = a.date.toDate();
+        return date.getFullYear() === year;
+      })
+    );
+  }, [activities, year]);
 
   if (code) {
     return <AuthCodePage code={code} />;
@@ -114,19 +125,38 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <AppBar position="fixed">
-        <Toolbar style={{ minHeight: '64px' }}>
+        <Toolbar style={{ minHeight: "64px" }}>
           <IconButton onClick={toggleMenu}>
             <MenuIcon />
           </IconButton>
-          <Menu id="menu" anchorEl={menuAnchor} open={Boolean(menuAnchor)} onClose={closeMenu}>
-            <MenuItem onClick={closeMenu}><NavButton to="/">Dashboard</NavButton></MenuItem>
-            <MenuItem onClick={closeMenu}><NavButton to="/recap">Recap</NavButton></MenuItem>
-            <MenuItem onClick={closeMenu}><NavButton to="/days">Active Days</NavButton></MenuItem>
-            <MenuItem onClick={closeMenu}><NavButton to="/map">Map</NavButton></MenuItem>
-            <MenuItem onClick={closeMenu} disabled={!profileIsAuthorized()}><NavButton to="/import">Import</NavButton></MenuItem>
+          <Menu
+            id="menu"
+            anchorEl={menuAnchor}
+            open={Boolean(menuAnchor)}
+            onClose={closeMenu}
+          >
+            <MenuItem onClick={closeMenu}>
+              <NavButton to="/">Dashboard</NavButton>
+            </MenuItem>
+            <MenuItem onClick={closeMenu}>
+              <NavButton to="/recap">Recap</NavButton>
+            </MenuItem>
+            <MenuItem onClick={closeMenu}>
+              <NavButton to="/days">Active Days</NavButton>
+            </MenuItem>
+            <MenuItem onClick={closeMenu}>
+              <NavButton to="/map">Map</NavButton>
+            </MenuItem>
+            <MenuItem onClick={closeMenu} disabled={!profileIsAuthorized()}>
+              <NavButton to="/import">Import</NavButton>
+            </MenuItem>
           </Menu>
           <Typography sx={{ flexGrow: 1 }}>Activity Visualizer</Typography>
-          <Select size="small" value={year} onChange={(e) => setYear(e.target.value)}>
+          <Select
+            size="small"
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+          >
             <MenuItem value={2023}>2023</MenuItem>
             <MenuItem value={2022}>2022</MenuItem>
             <MenuItem value={2021}>2021</MenuItem>
@@ -134,28 +164,39 @@ function App() {
             <MenuItem value={2019}>2019</MenuItem>
             <MenuItem value={2018}>2018</MenuItem>
           </Select>
-          {user && <>
-            <IconButton onClick={toggleAccount}>
-              <AccountCircleIcon />
-            </IconButton>
-            <Menu id="account" anchorEl={accountAnchor} open={Boolean(accountAnchor)} onClose={closeAccount}>
-              <MenuItem><NavButton to="/profile">Profile</NavButton></MenuItem>
-              <MenuItem onClick={logout}>Logout</MenuItem>
-            </Menu>
-          </>}
+          {user && (
+            <>
+              <IconButton onClick={toggleAccount}>
+                <AccountCircleIcon />
+              </IconButton>
+              <Menu
+                id="account"
+                anchorEl={accountAnchor}
+                open={Boolean(accountAnchor)}
+                onClose={closeAccount}
+              >
+                <MenuItem>
+                  <NavButton to="/profile">Profile</NavButton>
+                </MenuItem>
+                <MenuItem onClick={logout}>Logout</MenuItem>
+              </Menu>
+            </>
+          )}
         </Toolbar>
         <GuestDialog guest={guest} setGuest={setGuest} />
         <Wrapper>
-          {!user && !guest && <Switch>
-            <Route path="/guest/:id">
-              <Guest guest={guest} setGuest={setGuest} />
-            </Route>
-            <Route exact path="/">
-              <Login setUser={setUser} />
-            </Route>
-          </Switch>}
+          {!user && !guest && (
+            <Switch>
+              <Route path="/guest/:id">
+                <Guest guest={guest} setGuest={setGuest} />
+              </Route>
+              <Route exact path="/">
+                <Login setUser={setUser} />
+              </Route>
+            </Switch>
+          )}
 
-          {guest &&
+          {guest && (
             <Switch>
               <Route exact path="/guest/:id">
                 <Guest guest={guest} setGuest={setGuest} />
@@ -172,9 +213,10 @@ function App() {
               <Route path="/">
                 <Dashboard activities={yearActivities} year={year} />
               </Route>
-            </Switch>}
+            </Switch>
+          )}
 
-          {!guest && user &&
+          {!guest && user && (
             <Switch>
               <Route exact path="/guest/:id">
                 <Guest guest={guest} setGuest={setGuest} />
@@ -197,7 +239,8 @@ function App() {
               <Route path="/">
                 <Dashboard activities={yearActivities} year={year} />
               </Route>
-            </Switch>}
+            </Switch>
+          )}
         </Wrapper>
       </AppBar>
     </ThemeProvider>
